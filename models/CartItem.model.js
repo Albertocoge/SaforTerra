53315d9a28b0cf6daf06a302e13f20335cc7bd81
Alberto Codoñer Gea <alberto.cg2197@gmail.com>
2026-01-24 13:31:13 +0100

    feat(models): add CartItem Sequelize model

---
+++ models/CartItem.model.js
@@ -0,0 +1,44 @@
+// models/CartItem.model.js
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  const CartItem = sequelize.define(
+    "CartItem",
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      cartId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        field: "cart_id",
+      },
+
+      productId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        field: "product_id",
+      },
+
+      quantity: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        allowNull: false,
+        defaultValue: 1,
+        validate: { min: 1 },
+      },
+    },
+    {
+      tableName: "cart_items",
+      timestamps: true,
+      underscored: true,
+      indexes: [
+        { unique: true, fields: ["cart_id", "product_id"] }, // evita duplicados del mismo producto en el carrito
+      ],
+    }
+  );
+
+  return CartItem;
+};
